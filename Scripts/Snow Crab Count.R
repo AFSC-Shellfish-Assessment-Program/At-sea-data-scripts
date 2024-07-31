d8eaf060d218e5f18fcd0c021592c4cd649e4e6d
Erin-Fedewa-NOAA <erin.fedewa>
2024-07-30 21:26:26 -0800

    Updating 2024 crab count script

--- Scripts/Snow Crab Count.R
+++ Scripts/Snow Crab Count.R
@@ -1,19 +1,18 @@
-#Goal: Calculate 2023 running sum of snow crab caught on EBS BT survey 
-#Would be really awesome if we had a database like Metis that would enable pulling
-#of all survey leg data simultaneously real time vrs 6 access databases
-# ...but until that happens, here's a clunky script to track down our data
+#Goal: Calculate current year running sum of snow and tanner crab 
+  #caught on EBS BT survey 
+#NOTE: These are preliminary counts and have not been QA/QC'd! 
 
 #Author: Erin Fedewa
-#Last run: 8/8/23
 
 library(RODBC)
 library(tidyverse)
+library(ggridges)
+library(viridis) 
 library(patchwork)
 options(scipen = 999) #remove scientific notation from ggplot axes
 
 #Step 1: Download catch data
 #a) Download final access database for each vessel AFTER survey leg is completed 
-#M:\EBS Shelf\2022\RawData (I copied Access databases manually per leg/boat)
 #b) Download most recent FTP'd access database from ONGOING leg for both vessels 
 #https://sftp.afsc.noaa.gov/ThinClient/WTM/public/index.html#/login
 
@@ -26,30 +25,38 @@ import <- function(filename) {
 }
 
 #Add all databases
-AKK_L1 <- import("./Data/DataFromBoats/AKKN_2023_Leg1.accdb")
-AKK_L2 <- import("./Data/DataFromBoats/AKKN_2023_Leg2.accdb")
-AKK_L3 <- import("./Data/DataFromBoats/AKKN_2023_Leg3.accdb")
-AKK_L4 <- import("./Data/DataFromBoats/AKKN_2023_Leg4.accdb")
-NWX_L1 <- import("./Data/DataFromBoats/NWE_2023_Leg1.accdb")
-NWX_L2 <- import("./Data/DataFromBoats/NWE_2023_Leg2.accdb")
-NWX_L3 <- import("./Data/DataFromBoats/NWE_2023_Leg3.accdb")
+AKK_L1 <- import("./Data/DataFromBoats/AKKN_2024_Leg1.accdb")
+AKK_L2 <- import("./Data/DataFromBoats/AKKN_2024_Leg2.accdb")
+AKK_L3 <- import("./Data/DataFromBoats/AKKN_2024_Leg3.accdb")
+#AKK_L4 <- import("./Data/DataFromBoats/AKKN_2024_Leg4.accdb")
+NWX_L1 <- import("./Data/DataFromBoats/NWE_2024_Leg1.accdb")
+NWX_L2 <- import("./Data/DataFromBoats/NWE_2024_Leg2.accdb")
+NWX_L3 <- import("./Data/DataFromBoats/NWE_2024_Leg3.accdb")
 
-#Step 3: Merge data frames and sum number of snow crab caught (from tablet)
-bind_rows(AKK_L1, AKK_L2, AKK_L3, AKK_L4, NWX_L1, NWX_L2, NWX_L3) %>% 
+#Step 3: Drop non-standard stations (15:30 and slope/shelf)
+AKK_drop <- as.data.frame(c(1:17,23,25,45,133:140,152,154,169,170,209)) %>% setNames("HAUL")           
+NWE_drop <- as.data.frame(c(1:16,21,23,28:29,39,145,147,160,161)) %>% setNames("HAUL") 
+
+#Step 4: Merge data frames and sum number of snow crab caught (from tablet)
+bind_rows(AKK_L1, AKK_L2, AKK_L3, NWX_L1, NWX_L2, NWX_L3) %>% 
   filter(SPECIES_CODE == 68580,
-         CRUISE == 202301) %>%
-  summarise(Total = sum(NUMBER_CRAB)) -> snow23 #80,859 snow crab
+         CRUISE == 202401) %>%
+  filter(!(VESSEL == 162 & HAUL %in% AKK_drop$HAUL),
+                !(VESSEL == 134 & HAUL %in% NWE_drop$HAUL)) %>%
+  summarise(Total = sum(NUMBER_CRAB)) -> snow24 #220694 snow crab
 
 #Step 4: Merge data frames and sum number of tanner crab caught (from tablet)
-bind_rows(AKK_L1, AKK_L2, AKK_L3, AKK_L4, NWX_L1, NWX_L2, NWX_L3) %>% 
+bind_rows(AKK_L1, AKK_L2, AKK_L3, NWX_L1, NWX_L2, NWX_L3) %>% 
   filter(SPECIES_CODE == 68560,
-         CRUISE == 202301) %>%
-  summarise(Total = sum(NUMBER_CRAB)) -> tanner23 #33,208 tanner crab
+         CRUISE == 202401) %>%
+  filter(!(VESSEL == 162 & HAUL %in% AKK_drop$HAUL),
+         !(VESSEL == 134 & HAUL %in% NWE_drop$HAUL)) %>%
+  summarise(Total = sum(NUMBER_CRAB)) -> tanner24 #38697 tanner crab
 
 #Quick plot 
 #Append new data to timeseries 
-n <- read.csv("./Data/crab_n_timeseries.csv")
-new <- tibble(Year=2023, Snow_n=snow23[,1], Tanner_n=tanner23[,1])
+n <- read.csv("./Data/crab_n_timeseries.csv") %>% select(Year, Snow_n, Tanner_n)
+new <- tibble(Year=2024, Snow_n=snow24[,1], Tanner_n=tanner24[,1])
 n %>%
   full_join(new) -> crab_n
 
@@ -58,11 +65,11 @@ crab_n %>%
   mutate(section1 = if_else(Year < 2020, TRUE, FALSE),
          section2 = if_else(Year %in% c(2019:2021), TRUE, FALSE),
          section3 = if_else(Year > 2020, TRUE, FALSE),
-         section5 = if_else(Year < 2023, TRUE, FALSE),
-         section6 = if_else(Year == 2023, TRUE, FALSE)) %>%
+         section5 = if_else(Year < 2024, TRUE, FALSE),
+         section6 = if_else(Year == 2024, TRUE, FALSE)) %>%
   filter(!is.na(Snow_n)) -> df
 
-#Snow crab plot
+#Snow crab total count plot
 df %>%
   ggplot(aes(Year, Snow_n)) +
   geom_point(data = df %>% filter(section5 == TRUE), color = "blue", size=3) +
@@ -76,7 +83,7 @@ df %>%
   ggtitle("Snow Crab") +
   theme(plot.title = element_text(hjust = 0.5)) +
   theme(legend.position = "none", axis.title.x = element_blank()) +
-  scale_x_continuous(breaks = 2012:2023) -> snow
+  scale_x_continuous(breaks = 2012:2024) -> snow
 
 #Tanner crab plot
 df %>%
@@ -92,7 +99,7 @@ df %>%
   ggtitle("Tanner Crab") +
   theme(plot.title = element_text(hjust = 0.5)) +
   theme(legend.position = "none", axis.title.x = element_blank()) +
-  scale_x_continuous(breaks = 2012:2023) -> tanner
+  scale_x_continuous(breaks = 2012:2024) -> tanner
 
 #combine plots
 tanner/snow
@@ -109,31 +116,32 @@ import2 <- function(filename) {
 }
 
 #Add all databases
-AKK_L1_s <- import2("./Data/DataFromBoats/AKKN_2023_Leg1.accdb")
-AKK_L2_s <- import2("./Data/DataFromBoats/AKKN_2023_Leg2.accdb")
-AKK_L3_s <- import2("./Data/DataFromBoats/AKKN_2023_Leg3.accdb")
-AKK_L4_s <- import2("./Data/DataFromBoats/AKKN_2023_Leg4.accdb")
-NWX_L1_s <- import2("./Data/DataFromBoats/NWE_2023_Leg1.accdb")
-NWX_L2_s <- import2("./Data/DataFromBoats/NWE_2023_Leg2.accdb")
-NWX_L3_s <- import2("./Data/DataFromBoats/NWE_2023_Leg3.accdb")
+AKK_L1_s <- import2("./Data/DataFromBoats/AKKN_2024_Leg1.accdb")
+AKK_L2_s <- import2("./Data/DataFromBoats/AKKN_2024_Leg2.accdb")
+AKK_L3_s <- import2("./Data/DataFromBoats/AKKN_2024_Leg3.accdb")
+NWX_L1_s <- import2("./Data/DataFromBoats/NWE_2024_Leg1.accdb")
+NWX_L2_s <- import2("./Data/DataFromBoats/NWE_2024_Leg2.accdb")
+NWX_L3_s <- import2("./Data/DataFromBoats/NWE_2024_Leg3.accdb")
 
 #filter by males > 101mm 
-bind_rows(AKK_L1_s, AKK_L2_s, AKK_L3_s, AKK_L4_s, NWX_L1_s, NWX_L2_s, NWX_L3_s) %>% 
+bind_rows(AKK_L1_s, AKK_L2_s, AKK_L3_s, NWX_L1_s, NWX_L2_s, NWX_L3_s) %>% 
   filter(SPECIES_CODE == 68580,
          SEX == 1, 
-         WIDTH > 101,
-         CRUISE == 202301) %>%
-  count() -> pref #855 males > 101 vrs. 1000 in 2022, 
+         WIDTH >= 102,
+         CRUISE == 202401) %>%
+  filter(!(VESSEL == 162 & HAUL %in% AKK_drop$HAUL),
+         !(VESSEL == 134 & HAUL %in% NWE_drop$HAUL)) %>%
+  count() -> pref #878 males 
 
 #Number of PIBKC
-bind_rows(AKK_L1_s, AKK_L2_s, AKK_L3_s, AKK_L4_s, NWX_L1_s, NWX_L2_s, NWX_L3_s) %>% 
+bind_rows(AKK_L1_s, AKK_L2_s, AKK_L3_s, NWX_L1_s, NWX_L2_s, NWX_L3_s) %>% 
   filter(SPECIES_CODE == 69323,
          c(grepl(pattern="K|J|I|H|G|F|E",STATION))) %>%
   count() -> bkc #9
 
 #Plot for Buck and Cody 
 n <- read.csv("./Data/crab_n_timeseries.csv")
-new2 <- tibble(Year=2023, Snow_101_n=pref[,1], Prib_BKC=bkc[,1])
+new2 <- tibble(Year=2024, Snow_101_n=pref[,1], Prib_BKC=bkc[,1])
 n %>%
   full_join(new2) -> crab_n
 
@@ -161,3 +169,31 @@ crab_n %>%
 
 #combine plots
 blue/snow
+
+#Size Frequency distribution plots - snow crab 
+bind_rows(AKK_L1_s, AKK_L2_s, AKK_L3_s, NWX_L1_s, NWX_L2_s, NWX_L3_s) %>%  
+  filter(SPECIES_CODE == 68580,
+         CRUISE == 202401) %>%
+  filter(!(VESSEL == 162 & HAUL %in% AKK_drop$HAUL),
+         !(VESSEL == 134 & HAUL %in% NWE_drop$HAUL)) %>%
+  mutate(year = as.numeric(str_extract(CRUISE, "\\d{4}"))) %>%
+  ggplot() +
+  geom_density(aes(WIDTH)) +
+  scale_fill_viridis(name = "CW (mm)", option = "C") +
+  theme_bw() +
+  labs(x="Snow Crab Carapace Width", y="") -> snow_size
+
+#Size Frequency distribution plots - tanner crab 
+bind_rows(AKK_L1_s, AKK_L2_s, AKK_L3_s, NWX_L1_s, NWX_L2_s, NWX_L3_s) %>%  
+  filter(SPECIES_CODE == 68560,
+         CRUISE == 202401) %>%
+  filter(!(VESSEL == 162 & HAUL %in% AKK_drop$HAUL),
+         !(VESSEL == 134 & HAUL %in% NWE_drop$HAUL)) %>%
+  mutate(year = as.numeric(str_extract(CRUISE, "\\d{4}"))) %>%
+  ggplot() +
+  geom_density(aes(WIDTH)) +
+  scale_fill_viridis(name = "CW (mm)", option = "C") +
+  theme_bw() +
+  labs(x="Tanner Crab Carapace Width", y="") -> tanner_size
+
+snow_size/tanner_size
