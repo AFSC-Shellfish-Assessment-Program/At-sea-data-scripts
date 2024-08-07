e7c92d944a4f2e795926c7c167c880f5587333c1
Shannon Hennessey <s.hennessey13@gmail.com>
2024-08-07 08:35:01 -0800

    add "-B" slope stations to stations list

--- Scripts/BSFRF_opie_sampling.R
+++ Scripts/BSFRF_opie_sampling.R
@@ -26,7 +26,7 @@ haul <- read.csv("./Output/BSFRF_opilio_sampling/BSFRF_opie_haul_metadata.csv")
 
 
 # Visually check stations
-sort(unique(dat$STATION))
+# sort(unique(dat$STATION))
 
 # Create Look up table for shelf stratum 61 and slope subarea 6 stations (39 potential stations) 
 # Only 6 of shelf stratum 61 stations will have a side-by-side tow (but include catches from all standard stations),
@@ -34,8 +34,9 @@ sort(unique(dat$STATION))
 stations <- data.frame("R-32", "R-31", "Q-31", "P-32", "P-31", "O-31", "N-31", "M-32", "M-31", "L-31",
                        "R-32-B", "R-31-B", "Q-31-B", "P-32-B", "P-31-B", "O-31-B", "N-31-B", "M-32-B", "M-31-B", "L-31-B",
                        "61-15", "61-14", "61-18", "61-13", "61-12", "61-21", "61-07", "61-05", "61-06", "61-22", 
-                       "61-20", "61-03", "61-04", "61-17", "61-02", "61-01", "61-08", "61-09", "61-16") 
-                       # should there be subarea 6 "-B" stations?? I think so...
+                       "61-20", "61-03", "61-04", "61-17", "61-02", "61-01", "61-08", "61-09", "61-16",
+                       "61-15-B", "61-14-B", "61-18-B", "61-13-B", "61-12-B", "61-21-B", "61-07-B", "61-05-B", "61-06-B", "61-22-B", 
+                       "61-20-B", "61-03-B", "61-04-B", "61-17-B", "61-02-B", "61-01-B", "61-08-B", "61-09-B", "61-16-B") 
 
 # Standardize station naming, filter for stations 
 data <- dat %>%
@@ -45,13 +46,11 @@ data <- dat %>%
         select(VESSEL, HAUL, STATION, SPECIES_CODE, SEX, WIDTH, SHELL_CONDITION, SAMPLING_FACTOR) %>%
         mutate(VESSEL = case_when(VESSEL == 162 ~ "AKK",
                                   VESSEL == 134 ~ "NWE"))
-        # select(HAUL, STATION, SPECIES_CODE, SEX, LENGTH, WIDTH, SAMPLING_FACTOR) %>%
-        # right_join(., haul %>% select(-c(N_OPILIO)))
         # Ignore the warning message:
         # "Expected 3 pieces. Missing pieces filled with `NA`"
 
 haul2 <- data %>%
-         select(HAUL, STATION, SPECIES_CODE, SEX, LENGTH, WIDTH, SAMPLING_FACTOR) %>%
+         select(HAUL, STATION, SPECIES_CODE, SEX, WIDTH, SAMPLING_FACTOR) %>%
          right_join(., haul %>% select(-c(N_OPILIO_MALE))) %>%
          group_by(STATION, VESSEL) %>%
          summarise(N_OPILIO_MALE = sum(SAMPLING_FACTOR)) %>%
@@ -59,6 +58,7 @@ haul2 <- data %>%
          left_join(haul %>% select(-c(N_OPILIO_MALE)), .)
 
 write.csv(haul2, file="./Output/BSFRF_opilio_sampling/BSFRF_opie_haul_metadata.csv", row.names=FALSE)
+
 
 # Compute sum of all males by 1mm size bin   
 male_sizecomp <- data %>%
@@ -73,4 +73,6 @@ male_sizecomp <- data %>%
                  arrange(STATION, SIZE_BIN)
 
 write.csv(male_sizecomp, file="./Output/BSFRF_opilio_sampling/BSFRF_opie_allmale_size_comps.csv", row.names=FALSE)
+
+# WOULD IT BE HELPFUL TO POOL counts by station for side-by-side, rather than keep tows separate?
 
